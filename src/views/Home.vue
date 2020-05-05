6cb561331b98e0724e194c22ffe1f03b417514c9
Edcassola <edvaniosmith1999@gmail.com>
2020-05-05 17:11:55 +0100

    Starting to buid the structure of my project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,51 @@
 <template>
-      
+      <div id="app">
+          <header><h1>Welcome to G A R G U S</h1></header>
+          <main>
+              <section class="player">
+                  <h2 class="song-title">{{ current.title }}</h2>
+              </section>
+          </main>
+      </div>
 </template>
 
 <script>
+export default {
+    name: 'app',
+    data (){
+        return {
+            current: {
+                title: 'MUSIC TITLE'
+            },
+            songs: [
+                {
+                title: 'Stasera',
+                artists: 'Ultimo',
+                src: './assets/'
+                }
+            ]
+        }
+    }
+}
 
 </script>
 
 <style scoped>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
+body{
+    font-family: sens-serif;
+}
+
+header{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 15px;
+    background-color: #212121;
+}
 </style>
