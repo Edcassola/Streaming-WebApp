c28a62d72952f3e49da775616d612a276fa8fb04
Edcassola <edvaniosmith1999@gmail.com>
2020-05-07 06:22:28 +0100

    Finished the funcionalitym now I will take the UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,10 @@
               <section class="player">
                   <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
                   <div class="contro">
-                      <button class="prev">A Seguir</button>
+                      <button class="prev" @click="prev">prev</button>
                       <button class="play" v-if="!isPlaying" @click="play">Play</button>
                       <button class="pause" v-else @click="pause">Pause</button>
-                      <button class="next">Next</button>
+                      <button class="next" @click="next">Next</button>
                   </div>
               </section>
               <section class="playlist">
@@ -57,6 +57,24 @@ export default {
         pause () {
             this.player.pause();
             this.isPlaying = false;
+        },
+        next () {
+            this.index++;
+            if (this.index > this.songs.lenght -1) {
+                this.index = 0;
+            }
+
+            this.current = this.songs[this.index];
+            this.play(this.current)
+        },
+        prev () {
+            this.index--;
+            if (this.index < 0) {
+                this.index = this.songs.lenght - 1;
+            }
+
+            this.current = this.songs[this.index];
+            this.play(this.current);
         }
     },
     created () {
@@ -83,7 +101,7 @@ header{
     justify-content: center;
     align-items: center;
     padding: 15px;
-    background-color: #212121;
+    background-color: #6c63ff;
     color: #fff;
 }
 </style>
