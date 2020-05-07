1834f4d270919724680aca9824aa1954e0655ed5
Edcassola <edvaniosmith1999@gmail.com>
2020-05-07 04:32:06 +0100

    Adding 2 music's and create the Playlist

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,18 @@
           <header><h1>Welcome to G A R G U S</h1></header>
           <main>
               <section class="player">
-                  <h2 class="song-title">{{ current.title }} - <span>{{ current.title }}</span></h2>
+                  <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
                   <div class="contro">
                       <button class="prev">A Seguir</button>
-                      <button class="play" v-if="!isPlaying">Play</button>
-                      <button class="pause" v-else>Pause</button>
+                      <button class="play" v-if="!isPlaying" @click="play">Play</button>
+                      <button class="pause" v-else @click="pause">Pause</button>
                       <button class="next">Next</button>
                   </div>
+              </section>
+              <section class="playlist">
+                  <h3>The Playlist</h3>
+                  <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src == current.src) ? 'song playing' : 'song'">
+                  {{ song.title }} - {{ song.artist }} </button>
               </section>
           </main>
       </div>
@@ -22,17 +27,38 @@ export default {
         return {
             current: {},
             index: 0,
+            isPlaying: false,
             songs: [
                 {
-                    title: 'Motherless',
-                    artist: 'J.Cole',
-                    src: require('../assets/img/projota.mp3')
+                    title: 'Dior',
+                    artist: 'Pop Smoke',
+                    src: require('../assets/music/POP SMOKE - DIOR.mp3')
+                },
+                {
+                    title: 'Revenge',
+                    artist: 'Joyner Lucas',
+                    src: require('../assets/music/Joyner Lucas - Revenge.mp3')
                 }
             ],
             player: new Audio()
         }
     },
+    methods: {
+        play (song) {
+            if (typeof song.src != "undefined") {
+                this.current = song;
 
+                this.player.src = this.current.src;
+            }
+
+            this.player.play();
+            this.isPlaying = true;
+        },
+        pause () {
+            this.player.pause();
+            this.isPlaying = false;
+        }
+    },
     created () {
         this.current = this.songs[this.index];
         this.player.src = this.current.src;
